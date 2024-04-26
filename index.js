2f74507be60ce74f222b2ecdb5aa93c2d45fe56d
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-26 23:45:41 +0900

    jsonファイルをJSで読み込んでjsonファイルに書かれている内容をコンソール上に表示させることに成功

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict";
+// URLからデータを読み込む
+fetch("poke.json")
+
+// URLから読み込んだデータを受け取って加工する。受け取ったデータをresponceという引数に入れる
+.then(response => {
+    return response.json();//jsonはオブジェクト（連想配列）、jsonの部分をtextとかにするとテキスト変換される
+})
+
+// json変換したものをdateという引数に渡す
+.then(date =>{
+    console.log(date);
+})
+
+// 通信にエラーが発生した場合にこの関数が発生する
+.catch(error =>{
+    console.log(error);
+})
